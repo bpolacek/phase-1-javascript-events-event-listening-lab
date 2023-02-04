6c84ed39b41e674cf88e5ffe7a4ab5c230dbcfd2
Brett Polacek <brettpolacek@gmail.com>
2023-02-04 18:08:23 -0500

    added some index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    document.getElementById('button').addEventListener('click', callback);
+}
+//event listener to a found element
+
+//callback function
+function callback(e){
+    e.remove();
 }
